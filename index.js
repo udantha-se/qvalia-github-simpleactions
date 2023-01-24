b4654e9a0c74c4d08783bef8e8f7126fb4ed0596
Udantha Pathirana <udantha@ascentic.se>
2023-01-24 14:58:08 +0100

    11

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import * as os from 'os';
 
 try {
   const val = process.env[`INPUT_ACTION`] || '';
+  switch (val) {
+    case 'value':
+      require(val).run();
+      break;
+  
+    default:
+      console.log('Skipped because no action')
+      break;
+  }
   console.log('event ran')
 } catch (error) {
   process.exitCode = 1;
